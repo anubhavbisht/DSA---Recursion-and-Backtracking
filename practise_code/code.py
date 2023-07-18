e6fbb9e0769845e125edbbcbd87d44d5ece9cc72
Anubhav Bisht <40894333+anubhavbisht@users.noreply.github.com>
2023-07-18 16:43:08 +0530

    wip

--- practise_code/code.py
+++ practise_code/code.py
@@ -11,10 +11,51 @@ decreasingNumber(5)
 # ---------------------------------------------------------------------------- #
 #                 Print increasing number starting from given n                #
 # ---------------------------------------------------------------------------- #
-
 def increasingNumber(x):
   if (x==0):
     return
   increasingNumber(x-1)
   print(x)
 increasingNumber(10)
+
+# ---------------------------------------------------------------------------- #
+#              Print decreasing and increasing number from given n             #
+# ---------------------------------------------------------------------------- #
+def decreasingIncreasingNumber(x):
+  if (x==0):
+    return
+  print(x)
+  decreasingIncreasingNumber(x-1)
+  print(x)
+decreasingIncreasingNumber(5)
+
+# ---------------------------------------------------------------------------- #
+#                             Factorial of a number                            #
+# ---------------------------------------------------------------------------- #
+def factorial(x):
+  if (x==0):
+    return 1
+  return x * factorial(x-1)
+print(factorial(5))
+
+# ---------------------------------------------------------------------------- #
+#                               Power of a number                              #
+# ---------------------------------------------------------------------------- #
+def power(x,n):
+  if (n==0):
+    return 1
+  return x * power(x,n-1)
+print(power(2,5))
+
+# ---------------------------------------------------------------------------- #
+#               Power of a number in logarithmic time complexity               #
+# ---------------------------------------------------------------------------- #
+def powerInLogarithmicTime(x,n):
+  if (n==0):
+    return 1
+  result = powerInLogarithmicTime(x,n//2)
+  if (n%2==0):
+    return result*result
+  else:
+    return x*result*result
+print(powerInLogarithmicTime(2,5))
